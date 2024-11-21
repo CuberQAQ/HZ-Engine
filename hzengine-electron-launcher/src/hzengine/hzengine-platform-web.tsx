1ae86847dfed5d1a4384ae3c0bf6e2cae1c47c2f
CuberQAQ <a224503353@163.com>
2024-11-22 00:21:31 +0800

    1. Chore:hz-engine仓库依赖管理优化，readme完善；添加asuka-ui子模块 2. Feat: 添加 electron 相关

---
+++ hzengine-electron-launcher/src/hzengine/hzengine-platform-web.tsx
@@ -0,0 +1,81 @@
+import { HZEngineCore, Platform } from "hzengine-core";
+
+const HZEnginePlatformWeb: Platform = {
+    name: "",
+    getScreenSize: function (): [width: number, height: number] {
+      return [window.innerWidth, window.innerHeight];
+    },
+    createUILayer: function ({ z_index }: { z_index: number }): HTMLDivElement {
+      return document.createElement("div");
+    },
+    deleteUILayer: function (widgetFactory: HTMLDivElement): void {
+    },
+    isFileSync: function ({ path }: { path: string }): boolean {
+      
+      // Assets.
+      throw new Error("Function not implemented.");
+    },
+    readdirSync: function (
+      option: Platform.readdirSync.Option
+    ): string[] | undefined {
+      throw new Error("Function not implemented.");
+    },
+    statSync: function (
+      option: Platform.statSync.Option
+    ): Platform.statSync.FSStat | undefined {
+      throw new Error("Function not implemented.");
+    },
+    readFileSync: function (
+      option: Platform.readFileSync.Option
+    ): string | ArrayBuffer | undefined {
+      throw new Error("Function not implemented.");
+    },
+    writeFileSync: function (option: Platform.writeFileSync.Option): void {
+      throw new Error("Function not implemented.");
+    },
+    getImageInfo: function (img_path: string): {
+      width: number;
+      height: number;
+    } {
+      throw new Error("Function not implemented.");
+    },
+    getTime: function (): number {
+      return Date.now();
+    },
+    createAudioPlayer: function (): Platform.AudioPlayer {
+      return {
+        prepare: function (): void {
+          console.log(`[PlatformWeb] (Not implemented) prepare`);
+          
+        },
+        start: function (): void {
+          console.log(`[PlatformWeb] (Not implemented) start`);
+          
+        },
+        stop: function (): void {
+          console.log(`[PlatformWeb] (Not implemented) stop`);
+        },
+        release: function (): void {
+          console.log(`[PlatformWeb] (Not implemented) release`);
+        },
+        getMediaInfo: function (): Platform.MediaInfo {
+          console.log(`[PlatformWeb] (Not implemented) getMediaInfo`);
+          return {
+            artist: "",
+            title: "",
+            duration: 0
+          }
+        },
+        setSource: function ({ path }: { path: string }): void {
+          console.log(`[PlatformWeb] (Not implemented) setSource`);
+        },
+      };
+    },
+    releaseAudioPlayer: function (audio_player: Platform.AudioPlayer): void {
+    },
+    setFrameInterval: function (callback: () => void): void {
+      requestAnimationFrame(callback);
+    },
+}
+
+export default HZEnginePlatformWeb
--- readme.md
+++ readme.md
@@ -8,3 +8,42 @@
     - `watch_app2`: 一个 ZeppOS 小程序项目，草稿箱，想干啥都行
 - `hzengine-cli`: HZ-Engine CLI 脚手架，对应 npm 上发布的 hzengine-cli 包
 - `hzengine-core`: HZ-Engine Core 引擎核心代码
+
+## git clone 本项目后
+运行以下命令获取asuka-ui子模块
+```bash
+git submodule init
+git submodule update
+```
+
+## 开发环境：
+每个有 package.json 的文件夹都应该执行
+```bash
+npm i
+```
+pnpm, yarn 等均可
+
+### HZ-Engine ZeppOS Lacuncher 0.2
+注意 view 和 plugin 文件夹里的ts代码需要分别用 tsc 构建（之后用构建工具自动化）
+
+```bash
+npm i
+```
+编译 jsx 文件：
+```
+rollup -c
+```
+如果无需编译 jsx， 则无需使用 rollup
+
+即可直接用 zeus 打包 or 预览
+
+### HZ-Engine Electron Launcher
+
+#### electron 二进制包换源：
+```sh
+npm config edit
+```
+打开编辑器后，在空行添加：
+```
+electron_mirror=https://npmmirror.com/mirrors/electron/
+```
